3ce73587d53e0e668337f6e650cfa99ff20ff0da
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-05-11 11:35:58 +0900

    JSONパーサーを追加

---
+++ Gurunavi/Models/DataSource/JSONParser.swift
@@ -0,0 +1,12 @@
+//
+//  JSONParser.swift
+//  Gurunavi
+//
+//  Created by yuu ogasawara on 2017/05/11.
+//  Copyright © 2017年 smart tech ventures. All rights reserved.
+//
+
+import Foundation
+struct JSONParser {
+    let string = "33"
+}
