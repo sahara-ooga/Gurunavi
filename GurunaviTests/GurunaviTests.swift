dc8a81a3df4abf407b1e01d985c0296ac6b7f7e1
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-05-26 17:47:02 +0900

    不要なコメントを削除

--- GurunaviTests/GurunaviTests.swift
+++ GurunaviTests/GurunaviTests.swift
@@ -118,13 +118,6 @@ class GurunaviTests: XCTestCase {
         self.expectation?.fulfill()
     }
     
-//    func testPerformanceExample() {
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
-    
     func url() -> String {
         let keys = GurunaviKeys()
         
