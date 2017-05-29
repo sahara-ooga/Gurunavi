1cf2cf8298c67caddaa8d16a3d55710ae5098644
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-05-29 10:38:05 +0900

    AreaViewControllerに通知の解除を設定

--- Gurunavi/Controller/ViewController/AreaViewController.swift
+++ Gurunavi/Controller/ViewController/AreaViewController.swift
@@ -36,6 +36,9 @@ class AreaViewController: UIViewController {
                                                object: nil)
     }
     
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
 }
 
 extension AreaViewController:UITableViewDelegate{
